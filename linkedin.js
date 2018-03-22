bdc1633ca8d54d190da6c81f5bfc0a863b2e310f
tom8mot <tdo_terado@hotmail.com>
2018-03-22 21:30:11 +1100

    Add files via upload

---
+++ linkedin.js
@@ -0,0 +1,151 @@
+var inviter = {} || inviter;
+
+inviter.userList = [];
+
+inviter.className = 'button-secondary-small';
+
+
+
+inviter.refresh = function () {
+
+    window.scrollTo(0, document.body.scrollHeight);
+
+    window.scrollTo(document.body.scrollHeight, 0);
+
+    window.scrollTo(0, document.body.scrollHeight);
+
+};
+
+
+
+inviter.initiate = function()
+
+{
+
+    inviter.refresh();
+
+    var connectBtns = document.getElementsByClassName(inviter.className);
+
+
+
+    if (connectBtns == null) {var connectBtns = inviter.initiate();}
+
+    
+
+    return connectBtns;
+
+};
+
+inviter.invite = function () {
+
+    var connectBtns = inviter.initiate();
+
+    var buttonLength = connectBtns.length;
+
+    for (var i = 0; i < buttonLength; i++) {
+
+
+
+        if (connectBtns != null && connectBtns[i] != null) {inviter.handleRepeat(connectBtns[i]);}
+
+            
+
+        if (i == buttonLength - 1) {
+
+            console.log("done: " + i);
+
+            inviter.refresh();
+
+        }
+
+    }
+
+
+
+};
+
+
+
+inviter.handleRepeat = function(button)
+
+{
+
+    var nameValue = button.children[1].textContent
+
+    var name = nameValue.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+
+    if(inviter.arrayContains(name))
+
+    {
+
+         console.log("cancel");
+
+         var cancel = button.parentNode.parentNode.children[0];
+
+         cancel.click();
+
+    }
+
+    else
+
+    {
+
+        console.log("add");
+
+        inviter.userList.push(name);
+
+        button.click();
+
+         
+
+    }
+
+};
+
+
+
+inviter.arrayContains = function(item)
+
+{
+
+    return (inviter.userList.indexOf(item) > -1);
+
+};
+
+
+
+inviter.usersJson = {};
+
+
+
+inviter.loadResult = function()
+
+{
+
+ 
+
+    var retrievedObject = localStorage.getItem('inviterList');
+
+    var temp = JSON.stringify(retrievedObject);
+
+    inviter.userList = JSON.parse(temp);
+
+};
+
+
+
+inviter.saveResult = function()
+
+{
+
+    inviter.usersJson = JSON.stringify(inviter.userList);
+
+    localStorage.setItem('inviterList', inviter.usersJson);
+
+};
+
+
+
+
+
+setInterval(function () { inviter.invite(); }, 5000);
